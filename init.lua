314060172064b0cb5e66ba99cef5322ef5469838
ldaveyl <lucas.davey1@gmail.com>
2024-04-18 01:17:55 +0200

    Update

--- init.lua
+++ init.lua
@@ -33,16 +33,13 @@ vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ' }
 
-
 -- Column to indicate 80 line width
 -- vim.opt.colorcolumn = "80"
 
 -- Make cursor in insert mode fat
 -- vim.opt.guicursor = ""
 
-
-
--- [[[ KEYMAPS ]]
+-- [[ Keymaps ]]
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
@@ -56,17 +53,49 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- [[ Basic Autocommands ]]
+
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+require("lazy").setup({
+    
+    -- Detect tabstop and shiftwidth automatically
+    'tpope/vim-sleuth', 
+
+    -- "gc" to comment visual regions/lines
+    { 'numToStr/Comment.nvim', opts = {} }
 
 
-
-
-
-
-
-
-
-
-
+})
 
 
 
